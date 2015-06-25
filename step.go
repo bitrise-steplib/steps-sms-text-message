a7a7d6741eb38b9b6df4505abd2efdcb932ef062
Krisztian Goedrei <godreikrisztian@gmail.com>
2015-06-25 17:51:56 +0200

    step.go fixes

--- step.go
+++ step.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
@@ -9,109 +11,6 @@ import (
 
 	"./markdownlog"
 )
-
-const BASE_URL string = "https://api.twilio.com/2010-04-01"
-
-var (
-	accountSid   string
-	authToken    string
-	fromNumber   string
-	toNumber     string
-	message      string
-	errorMessage string
-)
-
-type Client struct {
-	AccountSid string
-	AuthToken  string
-	Uri        string
-}
-
-func NewClient(accountSid, authToken string) Client {
-	uri := fmt.Sprintf("%s/Accounts/%s/Messages.json", BASE_URL, accountSid)
-	fmt.Println("Client uri:", uri)
-
-	c := Client{AccountSid: accountSid, AuthToken: authToken, Uri: uri}
-
-	return c
-}
-
-func urlValuesFromMessageRequest(req MessageRequest) (url.Values, error) {
-	payload := url.Values{
-		"To":   {req.ToNumber},
-		"From": {req.FromNumber},
-		"Body": {req.Message},
-	}
-	return payload, nil
-}
-
-func (c *Client) PostMessage(req MessageRequest) error {
-	payload, err := urlValuesFromMessageRequest(req)
-	if err != nil {
-		return err
-	}
-	response_bytes := *strings.NewReader(payload.Encode())
-
-	/////////
-
-	client := &http.Client{}
-
-	request, _ := http.NewRequest("POST", c.Uri, &response_bytes)
-	request.SetBasicAuth(accountSid, authToken)
-	request.Header.Add("Accept", "application/json")
-	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
-	// Make request
-	resp, _ := client.Do(request)
-	fmt.Println(resp.Status)
-	/////////
-
-	/*
-		resp, err := http.PostForm(c.Uri, payload)
-		if err != nil {
-			return err
-		}
-		defer resp.Body.Close()
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return err
-		}
-
-		msgResp := &struct{ Status string }{}
-		if err := json.Unmarshal(body, msgResp); err != nil {
-			return err
-		}
-		if msgResp.Status != ResponseStatusSent {
-			return getError(body)
-		}
-	*/
-
-	return nil
-}
-
-type MessageRequest struct {
-	FromNumber string
-	ToNumber   string
-	Message    string
-}
-
-func buildMessageRequest(isBuildFailedMode bool) MessageRequest {
-	request := MessageRequest{
-		FromNumber: fromNumber,
-		ToNumber:   toNumber,
-	}
-
-	if isBuildFailedMode {
-		if errorMessage == "" {
-			fmt.Println("Build failed, but no TWILIO_SMS_ERROR_MESSAGE defined, use default")
-		} else {
-			message = errorMessage
-		}
-	}
-	request.Message = message
-
-	return request
-}
 
 func errorMessageToOutput(msg string) error {
 	message := "SMS send failed!\n"
@@ -145,47 +44,80 @@ func main() {
 
 	// input validation
 	// required
-	accountSid = os.Getenv("TWILIO_ACCOUNT_SID")
+	accountSid := os.Getenv("TWILIO_ACCOUNT_SID")
 	if accountSid == "" {
 		errorMessageToOutput("$TWILIO_ACCOUNT_SID is not provided!")
 		os.Exit(1)
 	}
-	authToken = os.Getenv("TWILIO_AUTH_TOKEN")
+	authToken := os.Getenv("TWILIO_AUTH_TOKEN")
 	if authToken == "" {
 		errorMessageToOutput("$TWILIO_AUTH_TOKEN is not provided!")
 		os.Exit(1)
 	}
-	toNumber = os.Getenv("TWILIO_SMS_TO_NUMBER")
+	toNumber := os.Getenv("TWILIO_SMS_TO_NUMBER")
 	if toNumber == "" {
 		errorMessageToOutput("$TWILIO_SMS_TO_NUMBER is not provided!")
 		os.Exit(1)
 	}
-	fromNumber = os.Getenv("TWILIO_SMS_TO_NUMBER")
+	fromNumber := os.Getenv("TWILIO_SMS_FROM_NUMBER")
 	if fromNumber == "" {
-		errorMessageToOutput("$TWILIO_SMS_TO_NUMBER is not provided!")
+		errorMessageToOutput("$TWILIO_SMS_FROM_NUMBER is not provided!")
 		os.Exit(1)
 	}
-	message = os.Getenv("TWILIO_SMS_MESSAGE")
+	message := os.Getenv("TWILIO_SMS_MESSAGE")
 	if message == "" {
 		errorMessageToOutput("$TWILIO_SMS_MESSAGE is not provided!")
 		os.Exit(1)
 	}
 	// optional
-	//TWILIO_SMS_ERROR_MESSAGE
-	errorMessage = os.Getenv("TWILIO_SMS_ERROR_MESSAGE")
+	errorMessage := os.Getenv("TWILIO_SMS_ERROR_MESSAGE")
 	if message == "" {
 		errorMessageToOutput("$TWILIO_SMS_ERROR_MESSAGE is not provided!")
 		os.Exit(1)
 	}
 
-	// perform step
 	isBuildFailedMode := (os.Getenv("STEPLIB_BUILD_STATUS") != "0")
-	req := buildMessageRequest(isBuildFailedMode)
-	c := NewClient(accountSid, authToken)
-	if err := c.PostMessage(req); err != nil {
-		errorMessageToOutput(err.Error())
-		os.Exit(1)
+	if isBuildFailedMode {
+		if errorMessage == "" {
+			fmt.Errorf("Build failed, but no TWILIO_SMS_ERROR_MESSAGE defined, use default")
+		} else {
+			message = errorMessage
+		}
 	}
 
-	successMessageToOutput(req.FromNumber, req.ToNumber, req.Message)
+	// Performing request
+	urlStr := "https://api.twilio.com/2010-04-01/Accounts/" + accountSid + "/Messages.json"
+
+	// Build out the data for our message
+	v := url.Values{}
+	v.Set("To", toNumber)
+	v.Set("From", fromNumber)
+	v.Set("Body", message)
+	rb := *strings.NewReader(v.Encode())
+
+	// Create client
+	client := &http.Client{}
+
+	req, _ := http.NewRequest("POST", urlStr, &rb)
+	req.SetBasicAuth(accountSid, authToken)
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	// Make request
+	resp, _ := client.Do(req)
+	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
+		fmt.Println(resp.Status)
+		successMessageToOutput(fromNumber, toNumber, message)
+
+		var data map[string]interface{}
+		bodyBytes, _ := ioutil.ReadAll(resp.Body)
+		err := json.Unmarshal(bodyBytes, &data)
+		if err == nil {
+			fmt.Println(data["sid"])
+
+		}
+	} else {
+		fmt.Println(resp.Status)
+		os.Exit(1)
+	}
 }
--- step.sh
+++ step.sh
@@ -1,73 +1,15 @@
 #!/bin/bash
 
-echo
-echo "TWILIO_ACCOUNT_SID: $TWILIO_ACCOUNT_SID"
-echo "TWILIO_AUTH_TOKEN: $TWILIO_AUTH_TOKEN"
-echo "TWILIO_SMS_TO_NUMBER: $TWILIO_SMS_TO_NUMBER"
-echo "TWILIO_SMS_FROM_NUMBER: $TWILIO_SMS_FROM_NUMBER"
-echo "TWILIO_SMS_MESSAGE: $TWILIO_SMS_MESSAGE"
-echo "TWILIO_SMS_MEDIA: $TWILIO_SMS_MEDIA"
+THIS_SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Required input validation
-# Account SID
-if [[ ! $TWILIO_ACCOUNT_SID ]]; then
-	echo
-    echo "No Account SID provided as environment variable. Terminating..."
-    echo
-    exit 1
-fi
+# Start go program
+cd "${THIS_SCRIPTDIR}"
 
-# Auth Token
-if [[ ! $TWILIO_AUTH_TOKEN ]]; then
-	echo
-    echo "No Auth Token provided as environment variable. Terminating..."
-    echo
-    exit 1
-fi
+go run ./step.go
+ex_code=$?
 
-# send to num
-if [[ ! $TWILIO_SMS_TO_NUMBER ]]; then
-	echo
-    echo "No phone number provided where to send the sms as environment variable. Terminating..."
-    echo
-    exit 1
-fi
-
-# send from num
-if [[ ! $TWILIO_SMS_FROM_NUMBER ]]; then
-	echo
-    echo "No phone number provided where the sms is coming from as environment variable. Terminating..."
-    echo
-    exit 1
-fi
-
-# TWILIO_SMS_MESSAGE
-if [[ ! $TWILIO_SMS_MESSAGE ]]; then
-	echo
-    echo "No text message provided as environment variable. Terminating..."
-    echo
-    exit 1
-fi
-
-######################
-
-res=$(curl -is -X POST "https://api.twilio.com/2010-04-01/Accounts/$TWILIO_ACCOUNT_SID/Messages.json" \
---data-urlencode "To=$TWILIO_SMS_TO_NUMBER"  \
---data-urlencode "From=$TWILIO_SMS_FROM_NUMBER"  \
---data-urlencode "Body=$TWILIO_SMS_MESSAGE" \
---data-urlencode "Media=$TWILIO_SMS_MEDIA" \
--u $TWILIO_ACCOUNT_SID:$TWILIO_AUTH_TOKEN)
-
-echo
-echo " --- Result ---"
-echo "$res"
-echo " --------------"
-
-http_code=$(echo "$res" | grep HTTP/ | awk {'print $2'} | tail -1)
-echo " [i] http_code: $http_code"
-
-if [ "$http_code" == "201" ]; then
+if [ ${ex_code} -eq 0 ] ; then
   exit 0
-else
-  exit 1
 fi
+
+exit 1
