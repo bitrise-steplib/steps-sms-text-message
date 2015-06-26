eca48ff6e25a49462806aca50b501e27fc12b36d
Krisztian Goedrei <godreikrisztian@gmail.com>
2015-06-26 11:11:53 +0200

    ci code style

--- _scripts/ci.sh
+++ _scripts/ci.sh
@@ -3,10 +3,13 @@
 set -e
 
 THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 #
 # Script for Continuous Integration
 #
+
 ${THIS_SCRIPT_DIR}/../step_test.sh
+
 #
 # ==> DONE - OK
 #
